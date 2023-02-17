93047ccdda0021b922aef7088bb1b2e934fae50d
Blek1 <bcmuxlow@gmail.com>
2023-02-17 01:11:40 -0800

    Created button with styling
    Created a simple button
    Added styling similar to the figma
    Changed the link of the review in the navbar so that you have to click on the button
    Open to other changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,15 +3,17 @@ import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 import SearchBar from './SearchBar'
+import Button from './Button'
 
 export default class Navbar extends Component {
   render() {
     return (
         <nav>
             <Link to="/"> Home </Link>
-            <Link to="/review"> Review </Link>
             <Link to="/login"> Login </Link>
             <SearchBar />
+            <Link to="/review"> <Button /> </Link>
+           
         </nav>
     )
   }
