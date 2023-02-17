b5539793ead920a44737672cc6070ae3dbf6f20d
superracer-hao <123527576+superracer-hao@users.noreply.github.com>
2023-02-16 22:04:02 -0800

    setup firebase db with blank home and login page
    pages are defined by the text in the top left

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,19 @@
-import bruinbanos from "./bruinbanos.png";
+import "./styles.css";
+import {BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-export default function App() {
-  
-  return <img src={bruinbanos} alt="bruinbanos logo" />
+import Home from "./pages/home";
+import Login from "./pages/login";
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
