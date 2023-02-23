b6f9ffdd61eca9db6e0980dc1746b559055b5004
Nathan Nguyendinh <nathannguyendinh1030@yahoo.com>
2023-02-23 01:37:35 -0800

    Added more data fields for 1-5 ratings

--- frontend/src/pages/review/Review.jsx
+++ frontend/src/pages/review/Review.jsx
@@ -4,8 +4,13 @@ import { db, auth } from '../../firebase-config';
 import { useNavigate } from 'react-router-dom';
 
 function Review( {isAuth} ) {
-  const [title, setTitle] = useState("");
+  const [bathroom, setBathroom] = useState("");
   const [reviewText, setReviewText] = useState("");
+  const [overall, setOverall] = useState("");
+  const [cleanliness, setCleanliness] = useState("");
+  const [comfort, setComfort] = useState("");
+  const [convenience, setConvenience] = useState("");
+  const [amenities, setAmenities] = useState("");
 
   // "reviews" is name of collection in Firebase
   const reviewsCollectionRef = collection(db, "reviews");
@@ -14,7 +19,12 @@ function Review( {isAuth} ) {
   // auth.currentUser.displayName and uid are google account information we can use
   const createReview = async () => {
     await addDoc(reviewsCollectionRef, {
-      title: title, 
+      bathroom: bathroom, 
+      overall: overall,
+      cleanliness: cleanliness,
+      comfort: comfort,
+      convenience: convenience,
+      amenities: amenities,
       reviewText: reviewText, 
       author: {name: auth.currentUser.displayName, id: auth.currentUser.uid}
     });
@@ -33,11 +43,56 @@ function Review( {isAuth} ) {
         <h1>Create A Post</h1>
       </div>
       <div>
-        <label> Title:</label>   
+        <label> Bathroom:</label>   
         <input 
-          placeholder='Title...' 
+          placeholder='Bathroom...' 
           onChange={(event) => {
-          setTitle(event.target.value)
+          setBathroom(event.target.value)
+          }}
+        />
+      </div>
+      <div>
+        <label> Overall:</label>   
+        <input 
+          placeholder='Overall Rating 1-5' 
+          onChange={(event) => {
+          setOverall(event.target.value)
+          }}
+        />
+      </div>
+      <div>
+        <label> Cleanliness:</label>   
+        <input 
+          placeholder='Cleanliness Rating 1-5' 
+          onChange={(event) => {
+          setCleanliness(event.target.value)
+          }}
+        />
+      </div>
+      <div>
+        <label> Comfort:</label>   
+        <input 
+          placeholder='Comfort Rating 1-5' 
+          onChange={(event) => {
+          setComfort(event.target.value)
+          }}
+        />
+      </div>
+      <div>
+        <label> Convenience:</label>   
+        <input 
+          placeholder='Convenience Rating 1-5' 
+          onChange={(event) => {
+          setConvenience(event.target.value)
+          }}
+        />
+      </div>
+      <div>
+        <label> Amenities:</label>   
+        <input 
+          placeholder='Amenities Rating 1-5' 
+          onChange={(event) => {
+          setAmenities(event.target.value)
           }}
         />
       </div>
