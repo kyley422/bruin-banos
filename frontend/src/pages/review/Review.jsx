9097b23f8443b1f7f0e6b93f1c7c46bd261d921c
Nathan Nguyendinh <nathannguyendinh1030@yahoo.com>
2023-02-23 01:14:16 -0800

    Initial Basic Firestore data store working

--- frontend/src/pages/review/Review.jsx
+++ frontend/src/pages/review/Review.jsx
@@ -1,7 +1,27 @@
-import React, { Component } from 'react'
-
+import React, { Component, useState } from 'react'
+import { addDoc, collection } from 'firebase/firestore'
+import { db, auth } from '../../firebase-config';
+import { useNavigate } from 'react-router-dom';
 
 function Review() {
+  const [title, setTitle] = useState("");
+  const [reviewText, setReviewText] = useState("");
+
+  // "reviews" is name of collection in Firebase
+  const reviewsCollectionRef = collection(db, "reviews");
+  let navigate = useNavigate();
+
+  // auth.currentUser.displayName and uid are google account information we can use
+  const createReview = async () => {
+    await addDoc(reviewsCollectionRef, {
+      title: title, 
+      reviewText: reviewText, 
+      author: {name: auth.currentUser.displayName, id: auth.currentUser.uid}
+    });
+    navigate("/");
+  };
+
+
   return (
     <div>
       <div>
@@ -9,13 +29,23 @@ function Review() {
       </div>
       <div className='inputGp'>
         <label> Title:</label>   
-        <input placeholder='Title...' />
+        <input 
+          placeholder='Title...' 
+          onChange={(event) => {
+          setTitle(event.target.value)
+          }}
+        />
       </div>
       <div className='inputGp'>
-        <label> Post:</label>
-        <textarea placeholder='Review...' />
+        <label> Review:</label>
+        <textarea 
+          placeholder='Review...' 
+          onChange={(event) => {
+          setReviewText(event.target.value)
+          }}
+        />
       </div>
-      <button>
+      <button onClick={createReview}>
         Submit Review
       </button>
 
