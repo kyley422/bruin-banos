f4aa864f71caf499449095ec73d5b019394e7d1d
Nathan Nguyendinh <30915871+nnguyendinh@users.noreply.github.com>
2023-03-03 04:43:01 -0800

    Hard-Coded Data Read for Dodd1737

--- frontend/src/pages/bathroom/BathroomPage.jsx
+++ frontend/src/pages/bathroom/BathroomPage.jsx
@@ -1,9 +1,33 @@
+import { async } from '@firebase/util';
+import { getDoc } from 'firebase/firestore';
 import React, { Component } from 'react'
 import { useParams } from 'react-router-dom';
+import { db } from '../../firebase-config';
+import { doc } from 'firebase/firestore';
+import { useEffect } from 'react';
 
 function BathroomPage() {
     const params = useParams()
     const bathroomId = params.bathroomId
+    const docRef = doc(db, "bathroom", "dodd1737")
+
+    useEffect(() => {
+        const getData = async () => {
+            const data = await getDoc(docRef)
+            console.log(data)
+
+            if (data.exists()) {
+                console.log("Document data:", data.data());  
+            } 
+            else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        };
+        
+        getData();
+    })
+
   return (
     <div>
       <div>Bathroom{bathroomId}</div>
