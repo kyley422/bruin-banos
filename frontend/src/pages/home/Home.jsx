a5bb9dc0c41f83ad36aa515d7035ffb34a840a52
kyley25 <kyle422@g.ucla.edu>
2023-03-05 01:13:03 -0800

    design filter bar gender selector

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,40 +1,70 @@
-import React, { Component } from 'react'
-import './Home.scss'
+import React, { Component } from "react";
+import "./Home.scss";
 
-import BathroomListings from '../../components/BathroomListings'
+import BathroomListings from "../../components/BathroomListings";
 
 const Hero = () => {
   return (
-    <div className='hero'>
-      <div className='image-container'>
-        <img src='https://i.imgur.com/vR0CIB2.png' alt='Bruin Baños Logo' />
+    <div className="hero">
+      <div className="image-container">
+        <img src="https://i.imgur.com/vR0CIB2.png" alt="Bruin Baños Logo" />
       </div>
-      <div className='text-container'>
-        <h1 className='hero-text'>Bruin Baños</h1>
-        <p className='hero-content'>Welcome to the ultimate resource for finding the best public bathrooms at UCLA! With detailed information on facilities, amenities, and user ratings, you can make informed decisions about where to go. So why wait? Start exploring and find the perfect bathroom for your needs today!</p>
+      <div className="text-container">
+        <h1 className="hero-text">Bruin Baños</h1>
+        <p className="hero-content">
+          Welcome to the ultimate resource for finding the best public bathrooms
+          at UCLA! With detailed information on facilities, amenities, and user
+          ratings, you can make informed decisions about where to go. So why
+          wait? Start exploring and find the perfect bathroom for your needs
+          today!
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const ListingContainer = () => {
   return (
-    <div className='listing-container'>
-      <div className='filter-container'>
-        Filters
+    <div className="listing-container">
+      <div className="filter-container">
+        <div className="filter-component">
+          <div className="filter-title">Filter</div>
+          <div className="gender-list">
+            <div className="checkbox-component">
+              <input type="checkbox" />
+              <label for="vehicle1">Male</label>
+            </div>
+            <div className="checkbox-component">
+              <input type="checkbox" />
+              <label for="vehicle2">Female</label>
+            </div>
+            <div className="checkbox-component">
+              <input type="checkbox" />
+              <label for="vehicle3">All Gender</label>
+            </div>
+          </div>
+        </div>
+        <div className="filter-component">
+          <div className="filter-title">Sort</div>
+          <div className="dropdown"></div>
+        </div>
+        <div className="filter-component">
+          <div className="filter-title">Range</div>
+          <div className="line-selector"></div>
+        </div>
       </div>
-    <BathroomListings />
+      <BathroomListings />
     </div>
-  )
-}
+  );
+};
 
 export default class Home extends Component {
   render() {
     return (
-      <div className='home'>
+      <div className="home">
         <Hero />
         <ListingContainer />
       </div>
-    )
+    );
   }
 }
