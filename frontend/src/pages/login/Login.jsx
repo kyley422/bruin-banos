629c7ce3a890c42b833b2fe869c76f9b89348bf8
Nathan Nguyendinh <30915871+nnguyendinh@users.noreply.github.com>
2023-03-04 17:16:34 -0800

    Set Up Basic "Create Doc Upon User Login"

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,14 +1,34 @@
 import { auth, provider } from "../../firebase-config"
 import { signInWithPopup } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom';
+import { db } from "../../firebase-config";
+import { collection, addDoc } from "firebase/firestore";
 
 function Login({ setIsAuth }) {
   let navigate = useNavigate();
+
+  const usersCollectionRef = collection(db, "users");
+  var reviews = ["id1", "id2", "id3"]
+  var likedReviews = ["id4", "id5", "id6"]
+
+  const createUserDoc = async () => {
+    await addDoc(usersCollectionRef, {
+      id: auth.currentUser.uid, 
+      name: auth.currentUser.displayName,
+      reviews: reviews,
+      likes: likedReviews
+    });
+    navigate("/");
+  };
 
   const signInWithGoogle = () => {
     signInWithPopup(auth, provider).then((result) => {
       localStorage.setItem("isAuth", true);
       setIsAuth(true)
+      // user_doc = getDoc(db, userID)
+      // if user_doc is none:
+      //    createUserDoc()
+      createUserDoc()
       navigate("/")
     });
   };
