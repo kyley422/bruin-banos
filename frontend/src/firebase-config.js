d7b4a1af877d4c827e2942bf9e19c6b3aa5b2067
Nathan Nguyendinh <30915871+nnguyendinh@users.noreply.github.com>
2023-03-05 22:16:22 -0800

    Updated to use backup Firebase Project

--- frontend/src/firebase-config.js
+++ frontend/src/firebase-config.js
@@ -1,3 +1,4 @@
+/*
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -17,6 +18,27 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+*/
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAIE2f8O7Zr8C1X7OvnhwbpT-I8_1pM2TM",
+  authDomain: "bruin-banos-2.firebaseapp.com",
+  projectId: "bruin-banos-2",
+  storageBucket: "bruin-banos-2.appspot.com",
+  messagingSenderId: "859286787012",
+  appId: "1:859286787012:web:714fd576f3d082ff478a91"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 
 export const db = getFirestore(app);
 export const auth = getAuth(app)
