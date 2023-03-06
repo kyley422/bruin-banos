d6afefa651b0ffec67cb3a4a3f0c0a4f3460dcd7
Tina Ma <tinatjma@gmail.com>
2023-03-05 23:00:12 -0800

    user page styling
    styled colors, fonts, button for google login prompt

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,57 +1,70 @@
-import { auth, provider } from "../../firebase-config"
-import { signInWithPopup } from 'firebase/auth'
-import { useNavigate } from 'react-router-dom';
+import { auth, provider } from "../../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 import { db } from "../../firebase-config";
 import { collection, addDoc, getDocs } from "firebase/firestore";
 import { query, where } from "firebase/firestore";
+
+import "./Login.scss";
+<link
+  href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap"
+  rel="stylesheet"
+></link>;
 
 function Login({ setIsAuth }) {
   let navigate = useNavigate();
 
   const usersCollectionRef = collection(db, "users");
-  
-  var reviews = []
-  var likedReviews = []
+
+  var reviews = [];
+  var likedReviews = [];
 
   const createUserDoc = async () => {
     await addDoc(usersCollectionRef, {
-      id: auth.currentUser.uid, 
+      id: auth.currentUser.uid,
       name: auth.currentUser.displayName,
       reviews: reviews,
-      likedReviews: likedReviews
+      likedReviews: likedReviews,
     });
     navigate("/");
   };
 
   const checkIfExists = async () => {
-    const q = query(usersCollectionRef, where('id', '==', auth.currentUser.uid))
-    const snapshot = await getDocs(q)
+    const q = query(
+      usersCollectionRef,
+      where("id", "==", auth.currentUser.uid)
+    );
+    const snapshot = await getDocs(q);
 
     // If user doc doesn't exist, create a new user doc
     if (snapshot.empty) {
-      console.log('User does not exist yet');
-      createUserDoc()
-    }  
-    else {
-      console.log('User already exists');
+      console.log("User does not exist yet");
+      createUserDoc();
+    } else {
+      console.log("User already exists");
     }
-  }
+  };
 
   const signInWithGoogle = () => {
     signInWithPopup(auth, provider).then((result) => {
       localStorage.setItem("isAuth", true);
-      setIsAuth(true)
-      checkIfExists()
-      navigate("/")
+      setIsAuth(true);
+      checkIfExists();
+      navigate("/");
     });
   };
 
   return (
-    <div className='loginPage'>
-      <p>Sign In With Google to Continue</p>
-      <button className='login-with-google-btn' onClick={signInWithGoogle}> 
-        Sign in with Google
-      </button>
+    <div className="loginPage">
+      <box className="background-box">
+        <box className="textbox">
+          <p>Log in / Sign Up</p>
+        </box>
+
+        <button className="login-with-google-btn" onClick={signInWithGoogle}>
+          Login with Google
+        </button>
+      </box>
     </div>
   );
 }
