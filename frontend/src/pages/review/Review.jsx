88f876047f1cbd332e6148bc0702dab0776f5710
Tina Ma <tinatjma@gmail.com>
2023-03-16 05:35:15 -0700

    centered button on logon page
    Please enter the commit message for your changes. Lines starting

--- frontend/src/pages/review/Review.jsx
+++ frontend/src/pages/review/Review.jsx
@@ -14,6 +14,7 @@ import { db, auth } from "../../firebase-config";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 import StarRating from "./StarRating";
+import { FaStar } from "react-icons/fa";
 import "./Review.scss";
 
 function Review({ isAuth }) {
